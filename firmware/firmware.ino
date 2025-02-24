78eb7ce8ae6e5dc8c1215207f20db4d6e0906452
wirbel <womp@herr-der-mails.de>
2025-02-24 20:13:02 +0100

    .

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,6 @@
 
 #include "NumberDisplay.h"
-
+#include "DS3231.h"
 
 /***************************************************************
  *      TFT        |  Nano
@@ -19,26 +19,66 @@ int8_t  CS_pins[] = { A6, A7, D7, D6, D5, D4, D3, D2 }; // left to right.
 NumberDisplay tft = NumberDisplay(CS_pins, sizeof(CS_pins)/sizeof(CS_pins[0]),
                                   TFT_CS, TFT_DC, TFT_RST);
 
+auto rtc = DS3231();
+DS3231::Time tm;
 
 void setup(void) {
-  tft.setSPISpeed(80000000); // 40 MHz
+  Serial.begin(115200);
+  delay(6000);
+  Serial.println("begin..");
+  //tft.setSPISpeed(80000000); // 40 MHz
   tft.init(240, 280, SPI_MODE0, true);
+  Serial.println("vor rtc.begin..");
+  rtc.begin();
+  Serial.println("initialized..");
 }
 
-uint32_t i=0;
+//uint32_t i=0;
+uint8_t Seconds { 100 };
 
 void loop(void) {
-  //Digits.SelectAll();
-  uint32_t n = i++;
+//  uint32_t n = i++;
+  Serial.println(" ..");
+  rtc.GetTime(&tm);
+//  if (tm.Seconds == Seconds)
+//     return;
+
+  char datestr[20];
+  rtc.PrintDateTime(&tm, datestr); Serial.println(datestr);
+
+
+
+  int8_t colon = -1 + (tm.Seconds & 1) * 10;
+
+  tft.DrawNumber(tm.Seconds % 10, 1); tm.Seconds /= 10;
+  tft.DrawNumber(tm.Seconds % 10, 0);
+
+  tft.DrawNumber(colon, 2);
+
+  tft.DrawNumber(tm.Minutes % 10, 4); tm.Minutes /= 10;
+  tft.DrawNumber(tm.Minutes % 10, 3);
+
+  tft.DrawNumber(colon, 5);
+
+  tft.DrawNumber(tm.Hours % 10, 4); tm.Hours /= 10;
+  tft.DrawNumber(tm.Hours % 10, 3);
+
+  /*struct Time {
+     uint8_t Seconds; // 00–59
+     uint8_t Minutes; // 00–59
+     uint8_t Hours;   // 00–23
+     uint8_t Days;    // 1–7
+     uint8_t Date;    // 01–31
+     uint8_t Month;   // 01-12
+     uint16_t Year;   // 20xx
+     };
 
   tft.DrawNumber(n % 10, 7); n /= 10;
   tft.DrawNumber(n % 10, 6); n /= 10;
   tft.DrawNumber(n % 10, 5); n /= 10;
   tft.DrawNumber(n % 10, 4); n /= 10;
-  tft.DrawNumber(n % 10, 3); n /= 10;
-  tft.DrawNumber(n % 10, 2); n /= 10;
-  tft.DrawNumber(n % 10, 1); n /= 10;
-  tft.DrawNumber(n % 10, 0); n /= 10;
+*/
+
   delay(50);
 
 }
